31e7de017606819f12f8682771dcdcacc714048e
amit.rai@evontech.com <anita.singh@cloudsufi.com>
2019-10-01 12:23:21 +0530

    initial commit

---
+++ src/screens/header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Header = props => {
+  const {headerTitle} = props;
+  return (
+    <View
+      style={{
+        backgroundColor: 'gray',
+        height: '30%',
+        paddingVertical: 10,
+        paddingHorizontal: 2,
+      }}>
+      <View
+        style={{
+          width: '100%',
+          position: 'absolute',
+          bottom: 0,
+          paddingVertical: 10,
+        }}>
+        <Text style={{alignSelf: 'center', color: 'white'}}>{headerTitle}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
