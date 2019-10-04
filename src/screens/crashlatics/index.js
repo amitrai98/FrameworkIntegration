c1ec6a27847c10651323bf025d2d8e0aa8c91ce3
amit.rai@evontech.com <anita.singh@cloudsufi.com>
2019-10-04 12:46:01 +0530

    added creashlytics

--- src/screens/crashlatics/index.js
+++ src/screens/crashlatics/index.js
@@ -2,12 +2,31 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import Header from '../header';
 type Props = {};
+import crashlytics from '@react-native-firebase/crashlytics';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export class CrashLatics extends Component<Props> {
+  crashTheApp(text) {
+    if (text != undefined) crashlytics().log(text);
+    else crashlytics().log('Testing crash');
+
+    crashlytics().crash();
+  }
   render() {
     return (
       <View>
         <Header headerText={'CrashLatics'} />
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              this.crashTheApp();
+            }}
+            style={{backgroundColor: 'green'}}>
+            <Text style={{paddingVertical: 10, paddingHorizontal: 15}}>
+              Crash Now
+            </Text>
+          </TouchableOpacity>
+        </View>
         <Text>CrashLatics page</Text>
       </View>
     );
